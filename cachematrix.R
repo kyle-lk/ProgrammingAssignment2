cbb7b6af6643da754613fd8bffe0c49a48c2c0ef
kyle <liukai_freesky@sohu.com>
2017-11-12 18:03:09 +0800

    cachematirx.R
    this is my coding work for R programming week 3 assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Hello, my name is kyle Liu, This is my mini program for week 3 assignment
+## create a function calculate inverse matrix by memory saving method
 
-## Write a short comment describing this function
 
+## I create 4 variables in function makeCacheMatrix(),each used to create an
+## memory site when load a matrix
+## makesure your input matrix is not singular
 makeCacheMatrix <- function(x = matrix()) {
-
+    InverseMatrix <- NULL
+    setMatrix <- function(y) {
+                 x <<- y
+                 InverseMatrix <<- NULL
+    }				 
+    get <- function() x
+	Inverse <- function(InverseM) InverseMatrix <<- InverseM
+	getInverseM <- function() InverseMatrix
+	list(setMatrix = setMatrix,
+	     get = get,
+		 Inverse = Inverse,
+		 getInverseM = getInverseM
+		 )
 }
 
 
-## Write a short comment describing this function
+## function cacheSolve() used to inverse matrix 'x'
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+              InverseMatrix <- x$getInverseM()
+			  if(!is.null(InverseMatrix)){
+			              message("getting cached data")
+						  return(InverseMatrix)
+			  }
+              matrix_x <- x$get()
+              InverseMatrix <- solve(matrix_x)
+              InverseMatrix			  
 }
